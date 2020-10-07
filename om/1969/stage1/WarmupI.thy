c99c2890cc1717814f9f8524936398277d6d64ec
Jakub Kądziołka <kuba@kadziolka.net>
2020-10-07 15:57:47 +0200

    warmup3

--- om/1969/stage1/WarmupI.thy
+++ om/1969/stage1/WarmupI.thy
@@ -2,9 +2,9 @@ theory %invisible WarmupI
   imports
     Complex_Main
     "HOL-Library.Sum_of_Squares"
-    "HOL-Number_Theory.Number_Theory"
+    "HOL-Library.Quadratic_Discriminant"
+    "HOL-Number_Theory.Cong"
     "HOL-Analysis.Analysis"
-    "HOL-Algebra.Algebra"
 begin
 
 section "Warmup problems"
@@ -184,6 +184,45 @@ proof -
   also have "?lhs = ((a + b)/2)^4" by algebra
   also have "... = (a+b)^4/16" using power_divide [of "a+b" 2, where n=4] by fastforce
   finally show ?thesis by auto
+qed
+
+subsection "Warmup 3"
+
+theorem
+  "\<bar>x-1\<bar>*\<bar>x+2\<bar>*\<bar>x-3\<bar>*\<bar>x+4\<bar> = \<bar>x+1\<bar>*\<bar>x-2\<bar>*\<bar>x+3\<bar>*\<bar>x-4\<bar>
+    \<longleftrightarrow> x \<in> {0, sqrt 7, -sqrt 7,
+                sqrt ((13 + sqrt 73) / 2),
+               -sqrt ((13 + sqrt 73) / 2),
+                sqrt ((13 - sqrt 73) / 2),
+               -sqrt ((13 - sqrt 73) / 2)}"
+  (is "?eqn \<longleftrightarrow> ?sols")
+proof -
+  have "?eqn \<longleftrightarrow> \<bar>(x-1)*(x+2)*(x-3)*(x+4)\<bar> = \<bar>(x+1)*(x-2)*(x+3)*(x-4)\<bar>" (is "_ \<longleftrightarrow> \<bar>?lhs\<bar> = \<bar>?rhs\<bar>")
+    by (simp add: abs_mult)
+  also have "... \<longleftrightarrow> ?lhs - ?rhs = 0 \<or> ?lhs + ?rhs = 0" by auto
+  also have "... \<longleftrightarrow> x*(x^2 - 7) = 0 \<or> x^4 - 13*x^2 + 24 = 0" by algebra
+  also have "x*(x^2 - 7) = 0 \<longleftrightarrow> x \<in> {0, sqrt 7, -sqrt 7}" using plus_or_minus_sqrt by auto
+  also have "x^4 - 13*x^2 + 24 = 0 \<longleftrightarrow> x\<^sup>2 \<in> {(13 + sqrt 73) / 2, (13 - sqrt 73) / 2}"
+    using discriminant_nonneg [where x="x^2", of 1 "-13" 24]
+    by (auto simp add: algebra_simps discrim_def)
+  also have "... \<longleftrightarrow> x \<in> {sqrt ((13 + sqrt 73) / 2),
+                         -sqrt ((13 + sqrt 73) / 2),
+                          sqrt ((13 - sqrt 73) / 2),
+                         -sqrt ((13 - sqrt 73) / 2)}"
+  proof -
+    have "0 \<le> (13 - sqrt 73) / 2" by (auto simp add: real_le_lsqrt)
+    hence "x\<^sup>2 = (13 - sqrt 73) / 2
+           \<longleftrightarrow> x \<in> {sqrt ((13 - sqrt 73) / 2),
+                    -sqrt ((13 - sqrt 73) / 2)}"
+      using plus_or_minus_sqrt
+      by blast
+    moreover have "x\<^sup>2 = (13 + sqrt 73) / 2
+      \<longleftrightarrow> x \<in> {sqrt ((13 + sqrt 73) / 2),
+              -sqrt ((13 + sqrt 73) / 2)}"
+      by (smt insert_iff power2_minus power_divide real_sqrt_abs real_sqrt_divide real_sqrt_pow2 singletonD)
+    ultimately show ?thesis by blast
+  qed
+  ultimately show ?thesis by blast
 qed
 
 end %invisible
