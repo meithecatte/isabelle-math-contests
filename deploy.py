7547ea0038fb3d0dd784fcf7e74ce521639d2e75
Jakub Kądziołka <kuba@kadziolka.net>
2020-10-09 01:37:41 +0200

    CI attempt 3

---
+++ deploy.py
@@ -0,0 +1,12 @@
+import os, shutil
+OUTDIR = 'output'
+for root, dirs, files in os.walk('.'):
+    if 'document.pdf' in files:
+        segments = root.split('/')
+        assert segments[0] == '.'
+        assert segments[-1] == 'output'
+        segments = segments[1:-1]
+        filename = '-'.join(segments) + '.pdf'
+        path = '/'.join([OUTDIR] + segments[:-1])
+        os.makedirs(path, exist_ok=True)
+        shutil.copy(root + '/document.pdf', path + '/' + filename)
